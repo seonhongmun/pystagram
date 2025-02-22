89405fbb4e53eda0c12d1e35dc86168210ebf09b
seonhongmun <mshg_@naver.com>
2025-02-22 22:35:10 +0900

    ✨ DB 설정

--- config/prod.py
+++ config/prod.py
@@ -3,3 +3,15 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['3.35.22.107', 'seonhm.kr']
+
+
+DATABASES = {
+    "default":{
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": SECRET['db']['name'],
+        "USER": SECRET['db']['user'],
+        "PASSWORD": SECRET['db']['password'],
+        "HOST": SECRET['db']['host'],
+        "PORT": "5432",
+    }
+}
